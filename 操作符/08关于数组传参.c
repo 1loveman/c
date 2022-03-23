3ce34f54de3de1bac7a338e285be95388d66d02a
1loveman <911137865@qq.com>
2022-03-23 16:16:10 +0800

    2022.03.23

---
+++ 操作符/08关于数组传参.c
@@ -0,0 +1,21 @@
+//#include<stdio.h>
+//
+//void print1(int arr3[]) {
+//	printf("%d\n", sizeof(arr3));
+//}
+//
+//void print2(char arr4[]) {//因为传递过来的都是地址，只占四个字节
+//	printf("%d\n", sizeof(arr4));
+//}
+//
+//int main() {
+//	int arr[10] = { 0 };
+//	char arr2[10] = { 0 };
+//	printf("%d\n", sizeof(arr));
+//	printf("%d\n", sizeof(arr2));
+//	print1(arr);
+//	print2(arr2);
+//
+//
+//	return 0;
+//}
