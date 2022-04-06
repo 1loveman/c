a8d50704aa67725a9d03c6f915516ad0775c090a
1loveman <911137865@qq.com>
2022-04-06 22:04:00 +0800

    2022.04.06

---
+++ 数据的存储/01.判断大小端.c
@@ -0,0 +1,18 @@
+//#include<stdio.h>
+//
+//int check_sys() {
+//	int a = 1;
+//	//小端返回1
+//	//大端返回0
+//	return *(char*)&a;
+//}
+//
+//int main() {
+//	int ret = check_sys();
+//	if (ret) {
+//		printf("小端\n");
+//	}
+//	else {
+//		printf("大端\n");
+//	}
+//}
