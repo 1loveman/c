2f912bbfc51957c48bcb27687669aca2e46e5030
1loveman <911137865@qq.com>
2022-04-11 21:58:20 +0800

    2022.04.11

---
+++ 数据的存储2/01.例题.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+
+int main() {
+	//题一
+	//unsigned int i;
+	//for (i = 9; i >= 0; i--) {//死循环,因为i范围为0--2^32-1
+	//	printf("%d\n", i);
+	//}
+	//题二
+	char a[1000];
+	int i;
+	for (i = 0; i < 1000; i++) {
+		a[i] = -1 - i;
+	}
+	printf("%d\n", strlen(a));//不会一直读取、因为a[i]范围为-128--0--+127;并且-128-1=127
+
+}
